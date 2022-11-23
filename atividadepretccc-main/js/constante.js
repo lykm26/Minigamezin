c4f67ad6e4e537de9616b6c37391796f6c40d8b3
lykm26 <79141488+lykm26@users.noreply.github.com>
2022-11-23 11:28:18 -0300

    Criando repositório

---
+++ atividadepretccc-main/js/constante.js
@@ -0,0 +1,38 @@
+"use strict"
+
+const COLUNA = 20;
+const LINHAS = 15;
+const SQUARO = 30;
+const COLORES = 
+	[
+		"#BFFF00" //chão
+		"#1f004d" //parede
+		"#ab0070" //personagem
+		"#ff0038" //inimigo
+	];
+	
+	Object.freeze(COLORES);
+	
+const CENARIO = 
+	[
+		[]
+		[[1, 1, 1, 1, 1]],
+		[[1, 1, 1]],
+		[[2, 2],
+		[2, 2], 
+		[2, 2],
+		[2, 2]]
+	]
+	
+	Object.freeze(CENARIO);
+	
+const PERSONAGENS = 
+	[
+		[]
+		[[3, 3],
+		[3, 3]],
+		[[4, 4]
+		[4, 4]]
+	]
+	
+	Object.freeze(PERSONAGENS);
